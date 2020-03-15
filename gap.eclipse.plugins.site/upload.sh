eae7df33b0dd6cdb8d4bc8e72b28e71434ab1bf6
gayanper <gayanper@gmail.com>
2020-03-15 19:22:21 +0100

    fix upload.sh package name

--- gap.eclipse.plugins.site/upload.sh
+++ gap.eclipse.plugins.site/upload.sh
@@ -5,7 +5,7 @@ ARCHIVE=$2
 VERSION=$3
 
 curl -X PUT -T $ARCHIVE -u gayanper:$API_KEY \
-        -H "X-Bintray-Package:gap-eclipse-jdt" \
+        -H "X-Bintray-Package:gap.eclipse.plugins" \
         -H "X-Bintray-Version:$VERSION" \
         -H "X-Bintray-Explode:1" \
         https://api.bintray.com/content/gayanper/p2/gap.eclipse.plugins/$VERSION
