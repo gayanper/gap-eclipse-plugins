03ce7c67fd9345f3bdffcc79b823e9960e9f0f68
gayanper <gayanper@gmail.com>
2020-03-15 15:26:16 +0100

    change the update site

--- gap.eclipse.plugins.site/upload.sh
+++ gap.eclipse.plugins.site/upload.sh
@@ -8,7 +8,7 @@ curl -X PUT -T $ARCHIVE -u gayanper:$API_KEY \
         -H "X-Bintray-Package:gap-eclipse-jdt" \
         -H "X-Bintray-Version:$VERSION" \
         -H "X-Bintray-Explode:1" \
-        https://api.bintray.com/content/gayanper/p2/gap-eclipse-jdt/$VERSION
+        https://api.bintray.com/content/gayanper/p2/gap.eclipse.plugins/$VERSION
 
-curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gap-eclipse-jdt/$VERSION/publish
+curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gap.eclipse.plugins/$VERSION/publish
 
