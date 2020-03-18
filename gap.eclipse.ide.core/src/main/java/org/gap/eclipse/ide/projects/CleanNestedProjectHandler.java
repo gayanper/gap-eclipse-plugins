5a33f920edc0cf505daaa4da3dc51c9d83aa9b16
gayanper <gayanper@gmail.com>
2020-03-18 18:15:40 +0100

    remove progress service from clean action

--- gap.eclipse.ide.core/src/main/java/org/gap/eclipse/ide/projects/CleanNestedProjectHandler.java
+++ gap.eclipse.ide.core/src/main/java/org/gap/eclipse/ide/projects/CleanNestedProjectHandler.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.internal.ide.dialogs.CleanDialog;
 
 @SuppressWarnings("restriction")
 public class CleanNestedProjectHandler extends BaseProjectHandler {
-
+	public CleanNestedProjectHandler() {
+		super(false);
+	}
+	
 	@Override
 	protected void executeOperation(List<IProject> projects, IProgressMonitor pm, IWorkbenchWindow window) {
 		CleanDialog cleanDialog = new CleanDialog(window, projects.toArray(new IProject[0]));
-		Display.getDefault().asyncExec(() -> cleanDialog.open());
+		cleanDialog.open();
 	}
 
 }
