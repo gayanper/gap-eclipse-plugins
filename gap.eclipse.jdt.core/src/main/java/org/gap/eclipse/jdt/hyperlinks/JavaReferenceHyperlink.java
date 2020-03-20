62510b7441149252e20f2950d82326b4e515ea3a
gayanper <gayanper@gmail.com>
2020-03-20 20:15:31 +0100

    find occurrence in file is done for private members

--- gap.eclipse.jdt.core/src/main/java/org/gap/eclipse/jdt/hyperlinks/JavaReferenceHyperlink.java
+++ gap.eclipse.jdt.core/src/main/java/org/gap/eclipse/jdt/hyperlinks/JavaReferenceHyperlink.java
@@ -1,14 +1,21 @@
 package org.gap.eclipse.jdt.hyperlinks;
 
+import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.internal.core.manipulation.search.OccurrencesFinder;
+import org.eclipse.jdt.internal.ui.search.FindOccurrencesEngine;
 import org.eclipse.jdt.internal.ui.search.JavaSearchQuery;
 import org.eclipse.jdt.ui.search.ElementQuerySpecification;
 import org.eclipse.jdt.ui.search.QuerySpecification;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.search.ui.NewSearchUI;
+import org.gap.eclipse.jdt.CorePlugin;
 import org.gap.eclipse.jdt.Messages;
 
 @SuppressWarnings("restriction")
@@ -40,9 +47,32 @@ class JavaReferenceHyperlink implements IHyperlink {
 
 	@Override
 	public void open() {
-		NewSearchUI.runQueryInBackground(new JavaSearchQuery(createQuery(element)));
+		if(element instanceof IMember) {
+			try {
+				final IMember member = (IMember) element;
+				if(Flags.isPrivate(member.getFlags())) {
+					findOccurenceInFile(member);
+				} else {
+					NewSearchUI.runQueryInBackground(new JavaSearchQuery(createQuery(element)));
+				}
+			} catch (JavaModelException e) {
+				CorePlugin.getDefault().logError(e.getMessage(), e);
+			}
+		} else {
+			NewSearchUI.runQueryInBackground(new JavaSearchQuery(createQuery(element)));
+		}
 	}
 	
+	private void findOccurenceInFile(IMember member) {
+		FindOccurrencesEngine engine= FindOccurrencesEngine.create(new OccurrencesFinder());
+		try {
+			ISourceRange range= member.getNameRange();
+			engine.run(member.getTypeRoot(), range.getOffset(), range.getLength());
+		} catch (JavaModelException e) {
+			CorePlugin.getDefault().logError(e.getMessage(), e);
+		}
+	}
+
 	private QuerySpecification createQuery(IJavaElement element) {
 		return new ElementQuerySpecification(element, IJavaSearchConstants.REFERENCES,
 				SearchEngine.createWorkspaceScope(), Messages.Hyperlink_SearchDescription);
