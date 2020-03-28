909de336e09134fe45da4e5ac8e714a744e8874c
gayanper <gayanper@gmail.com>
2020-03-28 19:39:42 +0100

    improved completion visitor working on overloaded methods.

--- gap.eclipse.jdt.core/src/main/java/org/gap/eclipse/jdt/types/CompletionASTVistor.java
+++ gap.eclipse.jdt.core/src/main/java/org/gap/eclipse/jdt/types/CompletionASTVistor.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -17,10 +18,12 @@ import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
 import org.eclipse.jface.text.BadLocationException;
 import org.gap.eclipse.jdt.CorePlugin;
@@ -65,28 +68,22 @@ class CompletionASTVistor extends ASTVisitor {
 		final CodeRange current = new CodeRange(node.getStartPosition(), node.getStartPosition() + node.getLength(),
 				node);
 
-		// for varargs they are considered as single parameter methods with a array.
-		int varArgsOffset = preceedSpace ? 2 : 1;
 		final IMethodBinding methodBinding = bindingSupplier.get();
 		if (methodBinding == null) {
 			return false;
 		}
 
-		offset = methodBinding.isVarargs() ? offset - varArgsOffset
-				: offset - (argumentSupplier.get().isEmpty() ? 0 : preceedSpace ? 2 : 1);
 		if (current.inRange(offset) && (lastVisited == null || lastVisited.inRange(current))) {
-			Set<ITypeBinding> typesAtOffset = findParameterTypeAtOffset(argumentSupplier, parameterSupplier, bindingSupplier);
-			typesAtOffset.stream()
-			.map(t -> {
+			Set<ITypeBinding> typesAtOffset = findParameterTypeAtOffset(argumentSupplier, parameterSupplier,
+					bindingSupplier, getDeclaringType(node));
+			typesAtOffset.stream().map(t -> {
 				try {
 					return project.findType(Signature.getTypeErasure(t.getQualifiedName()));
 				} catch (JavaModelException | IllegalArgumentException e) {
 					CorePlugin.getDefault().logError(e.getMessage(), e);
 					return null;
 				}
-			})
-			.filter(Predicates.notNull())
-			.forEach(t -> expectedTypes.add(t));
+			}).filter(Predicates.notNull()).forEach(t -> expectedTypes.add(t));
 			lastVisited = current;
 			return true;
 		}
@@ -94,7 +91,8 @@ class CompletionASTVistor extends ASTVisitor {
 	}
 
 	private Set<ITypeBinding> findParameterTypeAtOffset(Supplier<List<ASTNode>> argumentSupplier,
-			Function<IMethodBinding, List<ITypeBinding>> parameterSupplier, Supplier<IMethodBinding> bindingSupplier) {
+			Function<IMethodBinding, List<ITypeBinding>> parameterSupplier, 
+			Supplier<IMethodBinding> bindingSupplier, ITypeBinding containerType) {
 		final List<ASTNode> arguments = argumentSupplier.get();
 		final IMethodBinding binding = bindingSupplier.get();
 
@@ -105,58 +103,107 @@ class CompletionASTVistor extends ASTVisitor {
 		List<ITypeBinding> parameters = parameterSupplier.apply(binding);
 		if (arguments.isEmpty()) {
 			if (!parameters.isEmpty()) {
-				if (binding.isVarargs()) {
-					return Sets.newHashSet(parameters.get(0).getElementType());
-				} else {
-					return Sets.newHashSet(parameters.get(0));
-				}
+				return Sets.newHashSet(resolveType(parameters.get(0)));
 			}
 		} else {
 			int typeIndex = -1;
 			int checkOffset = preceedSpace ? offset - 1 : offset;
+			ITypeBinding lastType = null;
 
 			for (int i = 0; i < arguments.size(); i++) {
 				final ASTNode astNode = arguments.get(i);
 				if (astNode.getStartPosition() <= checkOffset
 						&& (astNode.getStartPosition() + astNode.getLength()) >= checkOffset) {
-					typeIndex = i + 1;
+					typeIndex = i;
 					break;
 				}
+				lastType = resolveTypeBinding(astNode);
 			}
 
-			if(binding != null) {
-				if ((typeIndex > -1) && typeIndex < parameters.size()) {
-					return Sets.newHashSet(parameters.get(typeIndex));
-				} else if (typeIndex == parameters.size()) {
-					// This might mean we are at a overloaded method. So lets look for overloads.
-					return findFromOverloaded(typeIndex, binding);
+			if (binding != null) {
+				final int pIndex = typeIndex;
+				final ITypeBinding lType = lastType;
+				List<IMethodBinding> overloads = findFromOverloaded(binding, containerType);
+				if (overloads.size() > 1) {
+					return overloads.stream().filter(m -> m.getParameterTypes().length >= pIndex + 1 || m.isVarargs())
+							.map(m -> {
+								if (m.isVarargs()) {
+									return resolveType(m.getParameterTypes()[m.getParameterTypes().length - 1]);
+								} else if (isNonGenericEqual(m.getParameterTypes()[pIndex - 1], lType)) {
+									return resolveType(m.getParameterTypes()[pIndex]);
+								}
+								return null;
+							})
+							.filter(Predicates.notNull())
+							.collect(Collectors.toSet());
+				} else {
+					if ((pIndex > -1) && pIndex < parameters.size()) {
+						return Sets.newHashSet(parameters.get(typeIndex));
+					} else if (binding.isVarargs()) {
+						return Sets.newHashSet(resolveType(parameters.get(parameters.size() - 1)));
+					}
+					return Collections.emptySet();
 				}
 			}
 		}
 		return Collections.emptySet();
 	}
 
-	private Set<ITypeBinding> findFromOverloaded(int typeIndex, IMethodBinding binding) {
+	private List<IMethodBinding> findFromOverloaded(IMethodBinding binding, ITypeBinding containerType) {
 		ITypeBinding type = binding.getDeclaringClass();
-     	return Arrays.stream(type.getDeclaredMethods())
-     		.filter(m -> {
-     			return m.getName().equals(binding.getName()) && !Modifier.isPrivate(m.getModifiers());
-     		})
-     		.filter(m -> m.getParameterTypes().length >= typeIndex + 1)
-     		.map(m -> m.getParameterTypes()[typeIndex])
-     		.collect(Collectors.toSet());
+		return Arrays.stream(type.getDeclaredMethods()).filter(m -> {
+			return m.getName().equals(binding.getName())
+					&& ((m.getDeclaringClass().equals(containerType) && Modifier.isPrivate(m.getModifiers()))
+							|| Modifier.isPublic(m.getModifiers()));
+		}).collect(Collectors.toList());
+	}
+	
+	private ITypeBinding resolveTypeBinding(ASTNode node) {
+		if (node instanceof Expression) {
+			return ((Expression) node).resolveTypeBinding();
+		}
+		return null;
+	}
+
+	private ITypeBinding resolveType(ITypeBinding binding) {
+		return (binding.isArray()) ? binding.getComponentType() : binding;
 	}
 
 	public Set<IType> getExpectedTypes() {
 		return expectedTypes;
 	}
-	
+
 	public IType getExpectedType() {
 		Iterator<IType> iterator = expectedTypes.iterator();
-		if(iterator.hasNext()) {
+		if (iterator.hasNext()) {
 			return iterator.next();
 		}
 		return null;
 	}
 	
+	public boolean isNonGenericEqual(ITypeBinding left, ITypeBinding right) {
+		 String leftName = Optional.ofNullable(left).map(ITypeBinding::getQualifiedName).map(this::genericErasure).orElse("");
+		 String rightName = Optional.ofNullable(right).map(ITypeBinding::getQualifiedName).map(this::genericErasure).orElse("");
+		 return leftName.equals(rightName);
+	}
+	
+	private String genericErasure(String fqn) {
+		int i = fqn.indexOf("<");
+		if(i > -1) {
+			return fqn.substring(0, i);
+		}
+		return fqn;
+	}
+	
+	private ITypeBinding getDeclaringType(ASTNode node) {
+		ASTNode n = node.getParent();
+		while(n != null) {
+			if(n instanceof TypeDeclaration) {
+				return ((TypeDeclaration) n).resolveBinding();
+			}
+			n = n.getParent();
+		}
+		return null;
+	}
+
 }
