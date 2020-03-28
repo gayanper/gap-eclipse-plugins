840b36be1020b8c5a8df17a649e591a11d44f7d6
gayanper <gayanper@gmail.com>
2020-03-28 20:22:37 +0100

    add unit tests for SmartEnumLiteralProposalComputerTest

--- gap.eclipse.jdt.test/src/main/java/org/gap/eclipse/jdt/types/SmartEnumLiteralProposalComputerTest.java
+++ gap.eclipse.jdt.test/src/main/java/org/gap/eclipse/jdt/types/SmartEnumLiteralProposalComputerTest.java
@@ -4,6 +4,7 @@ import static org.gap.eclipse.jdt.ProjectHelper.getCompilationUnit;
 import static org.gap.eclipse.jdt.ProjectHelper.getCompletionIndex;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -60,12 +61,7 @@ public class SmartEnumLiteralProposalComputerTest {
 		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
 		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
 		
-		List<String> expected = Arrays.asList("NEW : Thread.State - java.lang.Thread.Thread.State", 
-				"RUNNABLE : Thread.State - java.lang.Thread.Thread.State", 
-				"BLOCKED : Thread.State - java.lang.Thread.Thread.State", 
-				"WAITING : Thread.State - java.lang.Thread.Thread.State",
-				"TIMED_WAITING : Thread.State - java.lang.Thread.Thread.State", 
-				"TERMINATED : Thread.State - java.lang.Thread.Thread.State");
+		List<String> expected = expectedCompletions();
 		
 		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
 		assertTrue(actual.toString() ,expected.containsAll(actual));
@@ -87,12 +83,7 @@ public class SmartEnumLiteralProposalComputerTest {
 		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
 		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
 		
-		List<String> expected = Arrays.asList("NEW : Thread.State - java.lang.Thread.Thread.State", 
-				"RUNNABLE : Thread.State - java.lang.Thread.Thread.State", 
-				"BLOCKED : Thread.State - java.lang.Thread.Thread.State", 
-				"WAITING : Thread.State - java.lang.Thread.Thread.State",
-				"TIMED_WAITING : Thread.State - java.lang.Thread.Thread.State", 
-				"TERMINATED : Thread.State - java.lang.Thread.Thread.State");
+		List<String> expected = expectedCompletions();
 		
 		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
 		assertTrue(actual.toString() ,expected.containsAll(actual));
@@ -114,12 +105,7 @@ public class SmartEnumLiteralProposalComputerTest {
 		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
 		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
 		
-		List<String> expected = Arrays.asList("NEW : Thread.State - java.lang.Thread.Thread.State", 
-				"RUNNABLE : Thread.State - java.lang.Thread.Thread.State", 
-				"BLOCKED : Thread.State - java.lang.Thread.Thread.State", 
-				"WAITING : Thread.State - java.lang.Thread.Thread.State",
-				"TIMED_WAITING : Thread.State - java.lang.Thread.Thread.State", 
-				"TERMINATED : Thread.State - java.lang.Thread.Thread.State");
+		List<String> expected = expectedCompletions();
 		
 		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
 		assertTrue(actual.toString() ,expected.containsAll(actual));
@@ -141,17 +127,189 @@ public class SmartEnumLiteralProposalComputerTest {
 		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
 		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
 		
-		List<String> expected = Arrays.asList("NEW : Thread.State - java.lang.Thread.Thread.State", 
-				"RUNNABLE : Thread.State - java.lang.Thread.Thread.State", 
-				"BLOCKED : Thread.State - java.lang.Thread.Thread.State", 
-				"WAITING : Thread.State - java.lang.Thread.Thread.State",
-				"TIMED_WAITING : Thread.State - java.lang.Thread.Thread.State", 
-				"TERMINATED : Thread.State - java.lang.Thread.Thread.State");
+		List<String> expected = expectedCompletions();
 		
 		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
 		assertTrue(actual.toString() ,expected.containsAll(actual));
 	}
 
+	@Test
+	public void compute_EnumLiterals_On_VarArgs_FirstValue() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void test(java.lang.Thread.State... state) {\n");
+		code.append("  }\n");
+		code.append("  public void foo() {\n");
+		code.append("    test($)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = expectedCompletions();
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	@Test
+	public void compute_EnumLiterals_On_VarArgs_NthValue() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void test(java.lang.Thread.State... state) {\n");
+		code.append("  }\n");
+		code.append("  public void foo() {\n");
+		code.append("    test(Thread.State.WAITING, $)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = expectedCompletions();
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	@Test
+	public void compute_EnumLiterals_On_Overloaded_Methods() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void test(java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  public void test(java.nio.file.AccessMode mode) {\n");
+		code.append("  }\n");
+		code.append("  public void foo() {\n");
+		code.append("    test($)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = new ArrayList<>(expectedCompletions());
+		expected.add("READ : AccessMode - java.nio.file.AccessMode");
+		expected.add("WRITE : AccessMode - java.nio.file.AccessMode");
+		expected.add("EXECUTE : AccessMode - java.nio.file.AccessMode");
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	@Test
+	public void compute_EnumLiterals_On_Overloaded_Methods_OneParamMethod() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void test(java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  public void test(String name, java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  public void foo() {\n");
+		code.append("    test($)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = expectedCompletions();
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	@Test
+	public void compute_EnumLiterals_On_Overloaded_Methods_TwoParamMethod() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void test(java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  public void test(String name, java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  public void foo() {\n");
+		code.append("    test(\" \", $)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = expectedCompletions();
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	@Test
+	public void compute_EnumLiterals_On_Overloaded_Methods_PrivateMethods_SameClass() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void test(java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  private void test(String name, java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  public void foo() {\n");
+		code.append("    test(\" \", $)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = expectedCompletions();
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	@Test
+	public void compute_EnumLiterals_On_Overloaded_Methods_PrivateMethods_DifferentClass() throws Exception {
+		StringBuilder code = new StringBuilder();
+		code.append("package completion.test;\n");
+		code.append("public class SmartStatic {\n");
+		code.append("  public static void test(java.lang.Thread.State state) {\n");
+		code.append("  }\n");
+		code.append("  private static void test(java.nio.file.AccessMode mode) {\n");
+		code.append("  }\n");
+		code.append("public class SmartEnumLiteral {\n");
+		code.append("  public void foo() {\n");
+		code.append("    SmartStatic.test(\" \", $)\n");
+		code.append("  }\n");
+		code.append("}\n");
+		
+		int index = getCompletionIndex(code);
+		ICompilationUnit cu = getCompilationUnit(pkg, code, "SmartEnumLiteral.java");
+		List<ICompletionProposal> completions = computeCompletionProposals(cu, index);
+		
+		List<String> expected = expectedCompletions();
+		
+		List<String> actual = completions.stream().map(c -> c.getDisplayString()).collect(Collectors.toList());
+		assertTrue(actual.toString() ,expected.containsAll(actual));
+	}
+
+	private List<String> expectedCompletions() {
+		return Arrays.asList("NEW : Thread.State - java.lang.Thread.Thread.State", 
+				"RUNNABLE : Thread.State - java.lang.Thread.Thread.State", 
+				"BLOCKED : Thread.State - java.lang.Thread.Thread.State", 
+				"WAITING : Thread.State - java.lang.Thread.Thread.State",
+				"TIMED_WAITING : Thread.State - java.lang.Thread.Thread.State", 
+				"TERMINATED : Thread.State - java.lang.Thread.Thread.State");
+	}
+	
 	private List<ICompletionProposal> computeCompletionProposals(ICompilationUnit cu, int completionIndex) throws Exception {
 		SmartEnumLiteralProposalComputer comp= new SmartEnumLiteralProposalComputer();
 
