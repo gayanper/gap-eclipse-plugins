89691dc8d83bb2b8a727e5a5cf7148570fef9dd0
gayanper <gayanper@gmail.com>
2020-06-04 21:43:58 +0200

    improved reference hyperlink
    Signed-off-by: gayanper <gayanper@gmail.com>

--- gap.eclipse.jdt.core/src/main/java/org/gap/eclipse/jdt/hyperlinks/JavaReferenceHyperlinkDetector.java
+++ gap.eclipse.jdt.core/src/main/java/org/gap/eclipse/jdt/hyperlinks/JavaReferenceHyperlinkDetector.java
@@ -2,7 +2,10 @@ package org.gap.eclipse.jdt.hyperlinks;
 
 import java.util.List;
 
+import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaElementHyperlinkDetector;
 import org.eclipse.jdt.ui.actions.SelectionDispatchAction;
@@ -14,6 +17,15 @@ public class JavaReferenceHyperlinkDetector extends JavaElementHyperlinkDetector
 	@Override
 	protected void addHyperlinks(List<IHyperlink> hyperlinksCollector, IRegion wordRegion,
 			SelectionDispatchAction openAction, IJavaElement element, boolean qualify, JavaEditor editor) {
-		hyperlinksCollector.add(new JavaReferenceHyperlink(wordRegion, element));
+		
+		if(shouldProcess(element)) {
+			hyperlinksCollector.add(new JavaReferenceHyperlink(wordRegion, element));
+		}
+	}
+	
+	private boolean shouldProcess(IJavaElement element) {
+		return (element instanceof IType && !((IType) element).isResolved()) || 
+				(element instanceof IMethod && !((IMethod) element).isResolved()) ||
+				(element instanceof IField && !((IField) element).isResolved());
 	}
 }
