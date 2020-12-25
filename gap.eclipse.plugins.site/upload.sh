6a4d19be62214787deeab6a4b14c524e0b6aac63
Gayan Perera <gayanper@gmail.com>
2020-12-25 18:08:32 +0100

    Fix bintray upload

--- gap.eclipse.plugins.site/upload.sh
+++ gap.eclipse.plugins.site/upload.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 API_KEY=$1
-ARCHIVE=$2
-VERSION=$3
+API_UID=$2
+ARCHIVE=$3
+VERSION=$4
 
-curl -X PUT -T $ARCHIVE -u gayanper:$API_KEY \
+curl -X PUT -T $ARCHIVE -u $API_UID:$API_KEY \
         -H "X-Bintray-Package:gap.eclipse.plugins" \
         -H "X-Bintray-Version:$VERSION" \
         -H "X-Bintray-Explode:1" \
-        https://api.bintray.com/content/gayanper/p2/gap.eclipse.plugins/$VERSION
+        https://api.bintray.com/content/$API_UID/p2/gap.eclipse.plugins/$VERSION
 
-curl -X POST -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/gap.eclipse.plugins/$VERSION/publish
+curl -X POST -u$API_UID:$API_KEY https://api.bintray.com/content/$API_UID/p2/gap.eclipse.plugins/$VERSION/publish
 
